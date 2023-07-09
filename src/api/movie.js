c8bcb3af0367823d45f8a3e7a909101b46c22837
Abbiasone <119547052+Abbiasone@users.noreply.github.com>
2023-07-08 21:36:52 -0400

    Update movie.js

--- src/api/movie.js
+++ src/api/movie.js
@@ -1,6 +1,6 @@
 const getMovies = () => {
   const promise = new Promise((resolve, reject) => {
-    fetch(`https://www.omdbapi.com/?s=disney&apikey=cad57c10`)
+    fetch(`https://www.omdbapi.com/?s=marvel&apikey=cad57c10`)
       .then((dataJSON) => {
         const data = dataJSON.json();
         resolve(data);
