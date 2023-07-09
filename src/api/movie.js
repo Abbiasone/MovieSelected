bff900fc8273556b95692e920c4a22d61c69eac9
Abbiasone <119547052+Abbiasone@users.noreply.github.com>
2023-07-08 20:45:14 -0400

    Update movie.js
    se cambió de http a https de la api

--- src/api/movie.js
+++ src/api/movie.js
@@ -1,6 +1,6 @@
 const getMovies = () => {
   const promise = new Promise((resolve, reject) => {
-    fetch(`http://www.omdbapi.com/?s=spider-man&apikey=cad57c10`)
+    fetch(`https://www.omdbapi.com/?s=spider-man&apikey=cad57c10`)
       .then((dataJSON) => {
         const data = dataJSON.json();
         resolve(data);
